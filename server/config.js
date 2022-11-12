966e5d38841bd0cdc12807eaf609a09ee4fcfd24
Cody <cody.lehnhausen@gmail.com>
2022-11-11 17:57:32 -0700

    login lookin prettier

--- server/config.js
+++ server/config.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-const dotenv = require(`dotenv`);
-=======
 const dotenv = require(`dotenv`).config();
->>>>>>> master
 const flags = require(`flags`);
 flags.defineNumber(`port`, 3000);
 flags.parse();
 
 const port = flags.get(`port`) || process.env.PORT || 3000;
-<<<<<<< HEAD
+const secret = process.env.SECRET;
 
 module.exports = {
-    dotenv: dotenv,
-    http_port: port
-=======
-const secret = process.env.SECRET
-
-module.exports = {
-    http_port: port,
-    secret: secret,
->>>>>>> master
+  http_port: port,
+  secret: secret,
 };
